ac88d9769d1a1fc656074e05665103c78d70e213
Aaron Baw <aaronbaw@gmail.com>
2019-08-05 20:24:13 +0200

    Write programmatic rendering module with relative directory referencing

--- autorender.js
+++ autorender.js
@@ -2,50 +2,101 @@
  * Programmatic AutoRender Node.js module.
  */
 
-const { init, render } = require('@nexrender/core');
+const nexrender = require('@nexrender/core');
 const path = require('path');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
 require('dotenv').config({path: path.resolve(__dirname, './private/opts.env')});
 
 var nexrenderTemplate = require(path.resolve(__dirname, './nexrender_template.json'));
 
-if (!process.env.AE_TEMPLATE_PATH) throw new Error("AE_TEMPLATE_PATH env variable is not set.")
-  process.env.AE_TEMPLATE_PATH = './assets/STM_TEMPLATE_AUTORENDER_BUNDLED/STM_TEMPLATE_AUTORENDER_BUNDLED.aep';
-if (!process.env.AE_AUTORENDER_SCRIPT_PATH)
-  process.env.AE_AUTORENDER_SCRIPT_PATH = './scripts/stm_autorender_1.jsx';
+const DEFAULT_AE_TEMPLATE_PATH = './assets/STM_TEMPLATE_AUTORENDER_BUNDLED/STM_TEMPLATE_AUTORENDER_BUNDLED.aep';
+const DEFAULT_AE_AUTORENDER_SCRIPT_PATH = './scripts/stm_autorender_1.jsx';
+const DEFAULT_OUTPUT_PATH = './.output/';
 
-const AE_TEMPLATE_PATH = `file://${path.resolve(process.env.AE_TEMPLATE_PATH)}`;
-const AE_AUTORENDER_SCRIPT_PATH = `file://${path.resolve(process.env.AE_AUTORENDER_SCRIPT_PATH)}`;
+var AE_TEMPLATE_PATH = process.env.AE_TEMPLATE_PATH ? process.env.AE_TEMPLATE_PATH : DEFAULT_AE_TEMPLATE_PATH;
+var AE_TEMPLATE_URL = `file://${path.resolve(AE_TEMPLATE_PATH)}`;
+
+var AE_AUTORENDER_SCRIPT_PATH = process.env.AE_AUTORENDER_SCRIPT_PATH ? process.env.AE_AUTORENDER_SCRIPT_PATH : DEFAULT_AE_AUTORENDER_SCRIPT_PATH;
+var AE_AUTORENDER_SCRIPT_URL = `file://${path.resolve(AE_AUTORENDER_SCRIPT_PATH)}`;
+
+var OUTPUT_PATH = path.resolve(process.env.OUTPUT_PATH ? process.env.OUTPUT_PATH : DEFAULT_OUTPUT_PATH);
 
 const jobTemplate = require(path.resolve(__dirname, './nexrender_template.json'));
 
 // Configure the template by replacing placeholders with the script and asset paths.
-const configureJobTemplate = (jobTemplate,  outputPath) => {
+const configureJobTemplate = (jobTemplate, outputPath) => {
 
   var jobJson = { ...jobTemplate };
 
-  jobJson.template.src = AE_TEMPLATE_PATH;
-  jobJson.assets.src = AE_AUTORENDER_SCRIPT_PATH;
-  if (outputPath) jobJson.actions.postrender[0].output = path.resolve(outputPath);
+  jobJson.template.src = AE_TEMPLATE_URL;
+  jobJson.assets[0].src = AE_AUTORENDER_SCRIPT_URL;
+  jobJson.actions.postrender[0].output = OUTPUT_PATH;
 
   return jobJson;
 };
 
-const configureScriptTemplate = (backgroundPath, artworkPath, songPath) => {
-  
-}
+const configureScriptTemplate = ({projectName, backgroundPath, artworkPath, songPath, outputPath}) => new Promise((resolve, reject) => {
+  fs.readFile(path.resolve(AE_AUTORENDER_SCRIPT_PATH), (err, autorenderScriptTemplate) => {
+    autorenderScriptTemplate = autorenderScriptTemplate.toString('utf8');
+    // log(`Reading autorenderScriptTemplate:`, autorenderScriptTemplate);
+    // Write the script template to the output dir.
+    const finalScriptLocation = path.resolve(OUTPUT_PATH, projectName,  '.temp', `${projectName}_script.jsx`);
+    fs.writeFile(finalScriptLocation, autorenderScriptTemplate, err => {
+      if (err) reject(err);
+      log(`Written autorenderScriptTemplate to`, finalScriptLocation)
+      return resolve(finalScriptLocation);
+    });
+  });
+});
+
+// Configures the directory structure by creating the project folder and temp folders.
+const configureDirectoryStructure = (outputPath, projectName) => new Promise((resolve, reject) => {
+
+  // Set the new OUTPUT_PATH.
+  if (outputPath) OUTPUT_PATH = path.resolve(outputPath);
+
+  log(`Creating directory structure in`, path.resolve(OUTPUT_PATH, projectName, '.temp'));
+
+  mkdirp(path.resolve(OUTPUT_PATH, projectName, '.temp'), (err) => {
+    if (err) reject(err);
+    return resolve();
+  });
+});
 
 // Configure settings.
-const settings = init({
+const settings = nexrender.init({
     logger: console,
 });
 
 module.exports = {
-  render: function (backgroundPath, artworkPath, songPath, outputPath){
-    return new Promise((resolve, reject) => {
+  render: function (params){
+    return new Promise(async (resolve, reject) => {
 
+      var {projectName, backgroundPath, artworkPath, songPath, outputPath} = params;
+
+      if (!projectName) throw new Error("Project name not supplied.");
+
+      log(`Configuring directory structure.`);
+      await configureDirectoryStructure(outputPath, projectName);
+      log(`Configured directory structure:`, fs.readdirSync(OUTPUT_PATH));
+
+      log(`Configuring jobJson`);
       const jobJson = configureJobTemplate(jobTemplate, outputPath);
+      log(`Configured jobJson:`, jobJson);
 
+      log(`Configuring script template.`);
+      const workingScriptPath = await configureScriptTemplate(params);
+      log(`Saved + written script template.`);
+
+      log(`Attempting to render now.`);
+      const renderJob = await nexrender.render(jobJson, settings);
+      log(`Finished rendering`, renderJob);
 
     });
   }
+}
+
+function log(...msg){
+  console.log(`AUTORENDER |`, ...msg);
 }
