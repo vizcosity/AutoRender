1eb96d52eac0d511663351cace888c4a58e15073
Aaron Baw <aaronbaw@gmail.com>
2019-08-05 17:40:11 +0200

    Add STM Template bundled with all correctly linked files and assets, made portable

--- autorender.js
+++ autorender.js
@@ -9,12 +9,37 @@ require('dotenv').config({path: path.resolve(__dirname, './private/opts.env')});
 var nexrenderTemplate = require(path.resolve(__dirname, './nexrender_template.json'));
 
 if (!process.env.AE_TEMPLATE_PATH) throw new Error("AE_TEMPLATE_PATH env variable is not set.");
-if (!process.env.AE_AUTORENDER_SCRIPT) throw new Error("AE_AUTORENDER_SCRIPT env variable is not set.");
+if (!process.env.AE_AUTORENDER_SCRIPT_PATH) throw new Error("AE_AUTORENDER_SCRIPT_PATH env variable is not set.");
 
+const jobTemplate = require(path.resolve(__dirname, './nexrender_template.json'));
+
+// Configure the template by replacing placeholders with the script and asset paths.
+const configureJobTemplate = (jobTemplate,  outputPath) => {
+
+  var jobJson = { ...jobTemplate };
+
+  jobJson.template.src = process.env.AE_TEMPLATE_PATH;
+  jobJson.assets.src = process.env.AE_AUTORENDER_SCRIPT_PATH;
+  if (outputPath) jobJson.actions.postrender[0].output = outputPath;
+
+  return jobJson;
+};
+
+const configureScriptTemplate = (backgroundPath, artworkPath, songPath) => {
+  
+}
+
+// Configure settings.
+const settings = init({
+    logger: console,
+});
 
 module.exports = {
-  render: function (backgroundPath, artworkPath, songPath){
+  render: function (backgroundPath, artworkPath, songPath, outputPath){
     return new Promise((resolve, reject) => {
+
+      const jobJson = configureJobTemplate(jobTemplate, outputPath);
+
 
     });
   }
